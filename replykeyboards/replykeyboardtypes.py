bffff9d2a40efe61785091b214af00e56cb5a5cf
Sherzodbek Esanov <9669470@gmail.com>
2021-04-02 00:31:04 +0500

    Update replykeyboardtypes.py

--- replykeyboards/replykeyboardtypes.py
+++ replykeyboards/replykeyboardtypes.py
@@ -1,35 +1,90 @@
 from .replykeyboardvariables import *
 
 reply_keyboard_types = {
-
-    client_menu_keyboard: {
-        "uz": {
-            1: "Kitoblar",
-            2: "Buyurtmalarim",
-            3: "Biz bilan bog'lanish",
-            4: "Sozlamalar",
-            5: "🔥 MEGA AKSIYA ( 5 + 1 ) 🔥"
+    passenger_mail_keyboard: [
+        {
+            "text_uz": "Taksi qidirsh",
+            "text_ru": "Найди такси",
+            "text_cy": "Такси топиш",
+            "icon": "🚕",
+            "data": "search_taxi"
         },
-    },
+        {
+            "text_uz": "Yo'lovchi e'lon berish",
+            "text_ru": "Пассажир объявление",
+            "text_cy": "Йўловчи эълон бериш",
+            "icon": "🙋‍♂️",
+            "data": "announce_passanger"
+        },
+        {
+            "text_uz": "Pochta e'lon berish",
+            "text_ru": "Почта объявление",
+            "text_cy": "Почта эълон бериш",
+            "icon": "📦",
+            "data": "announce_mail"
+        },
+        {
+            "text_uz": "Aktiv e'lonlar",
+            "text_ru": "Активные объявления",
+            "text_cy": "Актив эълонлар",
+            "icon": "👁‍🗨",
+            "data": "active_announcements"
+        },
+        {
+            "text_uz": "Saqlanganlar",
+            "text_ru": "Сохраненные",
+            "text_cy": "Сақланганлар",
+            "icon": "🗄",
+            "data": "saved"
+        },
+        {
+            "text_uz": "Bosh menyu",
+            "text_ru": "Главное меню",
+            "text_cy": "Бош меню",
+            "icon": "🏠",
+            "data": "main_menyu"
+        },
 
-    admin_menu_keyboard: {
-        "uz": {1: "Yangi buyurtmalar", 2: "Qabul qilingan buyutmalar", 3: "Tarix", 4: "Sozlamalar"},
-    },
-
-    settings_keyboard: {
-        "uz": {1: "Mening ma'lumotlarim", 2: "Tilni o'zgartirish", 3: "Ortga"},
-        "cy": {1: "Менинг маълумотларим", 2: "Тилни ўзгартириш", 3: "Ортга"},
-        "ru": {1: "Мои данные", 2: "Изменить язык", 3: "Назад"},
-    },
-
-    phone_number_keyboard: {
-        "uz": {1: "Kontaktimni yuborish"},
-        "cy": {1: "Контактимни юбориш"},
-        "ru": {1: "Отправить мой контакт"}
-    },
-
-    location_keyboard: {
-        "uz": {1: 'keyingisi', 2: 'Geolokatsiyamni yuborish'}
-    }
+    ],
+    settings_keyboard: [
+        {
+            "text_uz": "Mening ma'lumotlarim",
+            "text_ru": "Мои данные",
+            "text_cy": "Менинг маълумотларим",
+            "icon": "",
+            "data": ""
+        },
+        {
+            "text_uz": "Tilni o'zgartirish",
+            "text_ru": "Изменить язык",
+            "text_cy": "Тилни ўзгартириш",
+            "icon": "",
+            "data": ""
+        },
+        {
+            "text_uz": "Ortga",
+            "text_ru": "Назад",
+            "text_cy": "Ортга",
+            "icon": "",
+            "data": ""
+        },
+    ],
+    phone_number_keyboard: [
+        {
+            "text_uz": "Telefon raqamini yuborish",
+            "text_ru": "Отправить номер телефона",
+            "text_cy": "Телефон рақамини юбориш",
+            "icon": "📱",
+            "data": ""
+        },
+    ],
+    location_keyboard: [
+        {
+            "text_uz": "Lokatsiyamni yuborish",
+            "text_ru": "Отправить мое местоположение",
+            "text_cy": "Локациямни юбориш",
+            "icon": "📍",
+            "data": ""
+        },
+    ]
 }
-
